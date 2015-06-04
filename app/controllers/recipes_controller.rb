7cb0ace21ee7b028d433d5f952ab66aed0473a77
Khoa Le <khoaminhle5@gmail.com>
2015-06-04 17:53:22 -0500

    add controller methods

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,34 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
+  def new
+  end
+
+  def create
+    @recipe = Recipe.create(title: params[:title], chef: params[:chef], ingredients: params[:ingredients], description: params[:description])
+    redirect_to "/recipes/#{@recipe.id}" 
+  end
+
+  def show
+    @recipe = Recipe.find_by(id: params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find_by(id: params[:id])
+    redirect_to "/recipes/#{@recipe.id}"
+  end
+
+  def update
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.update(title: params[:title], chef: params[:chef], ingredients: params[:ingredients], description: params[:description])
+    redirect_to "/recipes/#{@recipe.id}"
+  end
+
+  def destroy
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.destroy
+    redirect_to "/recipes"
+  end
+
 end
